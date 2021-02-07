15d17464e5b20927e33e2584a1cb31f8e33637cc
Dmytro Orgish <isnl.dd@gmail.com>
2021-02-07 16:59:30 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import debounce from 'lodash.debounce';
-import countryListTemp from './templates/country_list.hbs';
-import countryInfoTemp from './templates/country_info.hbs';
 import './css/styles.css';
-import PNotify from '../node_modules/pnotify/dist/es/PNotify.js';
-import '../node_modules/pnotify/dist/PNotifyBrightTheme.css';
 import fetchResult from './js/fetchCountries.js';
 
 const refInput = document.querySelector('input');
@@ -11,58 +7,6 @@ const refInput = document.querySelector('input');
 refInput.addEventListener(
   'input',
   debounce(event => {
-    fetchCountryData(event);
+    fetchResult(event);
   }, 500),
 );
-
-function fetchCountryData (event) {
-  const input = event.target.value;
-
-  fetchResult(input)
-    .then(response => {
-      if (response.status < 400) {
-        return response.json();
-      }
-    })
-    .then(data => {
-      removeList();
-      addList(data);
-      inputOutOfLimit(data);
-      countryInfo(data);
-    })
-    .catch(error => {
-      PNotify.error({
-        text: 'NO MATCHES!!!',
-      });
-    });
-}
-function countryInfo (data) {
-  if (data.length === 1) {
-    const markup = countryInfoTemp(data);
-    document.body.insertAdjacentHTML('beforeend', markup);
-  }
-}
-
-function inputOutOfLimit (data) {
-  if (data.length > 10) {
-    PNotify.error({
-      text: 'Too many matches found. Please enter a more specific query!',
-    });
-  }
-}
-
-function removeList () {
-  if (document.querySelector('.country-list')) {
-    document.querySelector('.country-list').remove();
-  }
-  if (document.querySelector('.info')) {
-    document.querySelector('.info').remove();
-  }
-}
-
-function addList (data) {
-  if (data.length > 1 && data.length <= 10) {
-    const markup = countryListTemp(data);
-    document.body.insertAdjacentHTML('beforeend', markup);
-  }
-}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,74 @@
-const url = 'https://restcountries.eu/rest/v2/name';
+import countryListTemp from '../templates/country_list.hbs';
+import countryInfoTemp from '../templates/country_info.hbs';
+import PNotify from '../../node_modules/pnotify/dist/es/PNotify.js';
+import '../../node_modules/pnotify/dist/PNotifyBrightTheme.css';
 
-export default function fetchCountries (searchQuery) {
+export default function fetchCountryData (event) {
+  const input = event.target.value;
+
+  fetchCountries(input)
+    .then(response => {
+      console.log(response.status);
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
+      }
+    })
+    .then(data => {
+      removeList();
+      addList(data);
+      inputOutOfLimit(data);
+      countryInfo(data);
+    })
+    .catch(error => {
+      messageWarn(event);
+    });
+}
+
+function messageWarn (event) {
+  if (event.target.value.length > 1) {
+    PNotify.error({
+      text: 'NO MATCHES!!!TRY TO TYPE ANOTHER VALUE!!!',
+
+      delay: 1500,
+      closerHover: true,
+    });
+  }
+}
+
+function fetchCountries (searchQuery) {
+  const url = 'https://restcountries.eu/rest/v2/name';
   return fetch(`${url}/${searchQuery}`);
+}
+
+function countryInfo (data) {
+  if (data.length === 1) {
+    const markup = countryInfoTemp(data);
+    document.body.insertAdjacentHTML('beforeend', markup);
+  }
+}
+
+function inputOutOfLimit (data) {
+  if (data.length > 10) {
+    PNotify.error({
+      text: 'Too many matches found. Please enter a more specific query!',
+      delay: 1500,
+      closerHover: true,
+    });
+  }
+}
+
+function removeList () {
+  if (document.querySelector('.country-list')) {
+    document.querySelector('.country-list').remove();
+  }
+  if (document.querySelector('.info')) {
+    document.querySelector('.info').remove();
+  }
+}
+
+function addList (data) {
+  if (data.length > 1 && data.length <= 10) {
+    const markup = countryListTemp(data);
+    document.body.insertAdjacentHTML('beforeend', markup);
+  }
 }
